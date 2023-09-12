03c18f70f3f6731ae4985d81a164492106850a51
Sait Karaca <sait.karaca1@gmail.com>
2023-09-12 22:29:31 +0300

    Button Controller Added

--- dcMotorController.ino
+++ dcMotorController.ino
@@ -3,23 +3,29 @@ const int dirPin = 2;
 const int stepPin = 3;
 const int stepsPerRevolution = 200; 
 
-int led=11 ;
-int speedUpButton=5 ;
-int speedDownButton=9 ;
+const int sensor = A0 ;
+
+int speedUpButton=6 ;
+int speedDownButton=7 ;
 int startDuration = 3000 ;
+
+int autoModButton=5 ;
+int manuelModButton=9 ;
 
 void setup()
 {
   // Declare pins as Outputs
-  pinMode(led, OUTPUT);
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   // Declare pins as Inputs
   pinMode(speedUpButton,INPUT);
   pinMode(speedDownButton,INPUT);
+  pinMode(autoModButton,INPUT);
+  pinMode(manuelModButton,INPUT);
 }
 void loop()
 {
+  // ----> Speed Settings
 
   static int duration = startDuration;
 
@@ -27,22 +33,54 @@ void loop()
   int speedDownButtonState=digitalRead(speedDownButton);
 
   if (speedUpButtonState == HIGH){
-    if (duration > 2000) {
-    duration -= 1000 ;
-    digitalWrite(led,HIGH);
-    }
-  }else {
-    digitalWrite(led,LOW);
+      if (duration > 2000) {
+      duration -= 1000 ;
+      }
+  }
+   if (speedDownButton == HIGH){
+      if (duration < 4000) {
+      duration += 1000 ;
+      }
   }
 
-   if (speedDownButton == HIGH){
-    if (duration < 4000) {
-    duration += 1000 ;
-    digitalWrite(led,HIGH);
-    }
-  }else {
-    digitalWrite(led,LOW);
-  }
+  // ----> Mod select
+  int autoModButtonState=digitalRead(autoModButton);
+  int manuelModButtonState=digitalRead(manuelModButton);
+  int level=analogRead(sensor);
+
+  if (autoModButtonState == HIGH){ // ----> Auto Mod
+
+      if (level > 500){ // Sound Level 
+
+        for(int x=1;x<=10;x++){ // Number of Laps
+
+        digitalWrite(dirPin, LOW); // Set motor direction clockwise
+
+        // Spin motor
+        for(int x = 0; x < stepsPerRevolution; x++)
+        {
+          digitalWrite(stepPin, HIGH);
+          delayMicroseconds(duration); 
+          digitalWrite(stepPin, LOW);
+          delayMicroseconds(duration); 
+        }
+        delay(50); 
+        digitalWrite(dirPin, HIGH); // Set motor direction counterclockwise
+
+        // Spin motor
+        for(int x = 0; x < stepsPerRevolution; x++)
+        {
+          digitalWrite(stepPin, HIGH);
+          delayMicroseconds(duration); 
+          digitalWrite(stepPin, LOW);
+          delayMicroseconds(duration); 
+        }
+        delay(50); 
+      }
+       delay(10000); 
+      }
+
+  }else if (manuelModButtonState == HIGH){ // ----> Manuel Mod
 
   digitalWrite(dirPin, LOW); // Set motor direction clockwise
 
@@ -66,5 +104,6 @@ void loop()
     delayMicroseconds(duration); 
   }
   delay(50);
-
+  }
+  
 }
